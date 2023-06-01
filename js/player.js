4fbc2504922b6e96b4d61891d8e17626aa860a31
jlheard <heardjasonl@gmail.com>
2023-05-31 22:24:29 -0400

    Add logic to show invalid plays when populating player's hand

--- js/player.js
+++ js/player.js
@@ -24,36 +24,45 @@ export class Player {
     }
 
     populateHandElement(handElement) {
-        if (!handElement) {
-            return;
+      if (!handElement) {
+        return;
+      }
+    
+      handElement.innerHTML = '';
+    
+      const cardsBySuit = {};
+      this.hand.cards.forEach(card => {
+        if (!cardsBySuit[card.suit]) {
+          cardsBySuit[card.suit] = [];
         }
-
-        handElement.innerHTML = '';
-
-        const cardsBySuit = {};
-        this.hand.cards.forEach(card => {
-            if (!cardsBySuit[card.suit]) {
-                cardsBySuit[card.suit] = [];
-            }
-            cardsBySuit[card.suit].push(card);
+        cardsBySuit[card.suit].push(card);
+      });
+    
+      const validPlaysMap = this.hand.getLegalPlaysMap(null, false);
+      const validPlays = Array.from(validPlaysMap.values());
+    
+      Object.values(cardsBySuit).forEach(cards => {
+        cards.sort((a, b) => RANK_ORDER.indexOf(a.rank) - RANK_ORDER.indexOf(b.rank));
+    
+        const cardGroup = document.createElement('div');
+        cardGroup.classList.add('card-group');
+    
+        cards.forEach(card => {
+          const cardElement = document.createElement('div');
+          cardElement.classList.add('card');
+          cardElement.classList.add('south');
+          cardElement.classList.add(`suit-${card.suit.toLowerCase()}`);
+          cardElement.innerHTML = `<div class="card-content">${card.rank}&nbsp;${getSuitSymbol(card.suit)}</div>`;
+    
+          // Check if the card value exists in validPlays
+          if (validPlays.includes(card)) {
+            cardElement.classList.add('valid-play');
+          }
+    
+          cardGroup.appendChild(cardElement);
         });
-
-        Object.values(cardsBySuit).forEach(cards => {
-            cards.sort((a, b) => RANK_ORDER.indexOf(a.rank) - RANK_ORDER.indexOf(b.rank));
-
-            const cardGroup = document.createElement('div');
-            cardGroup.classList.add('card-group');
-
-            cards.forEach(card => {
-                const cardElement = document.createElement('div');
-                cardElement.classList.add('card');
-                cardElement.classList.add('south');
-                cardElement.classList.add(`suit-${card.suit.toLowerCase()}`);
-                cardElement.innerHTML = `<div class="card-content">${card.rank}&nbsp;${getSuitSymbol(card.suit)}</div>`;
-                cardGroup.appendChild(cardElement);
-            });
-
-            handElement.appendChild(cardGroup);
-        });
-    }
+    
+        handElement.appendChild(cardGroup);
+      });
+    } 
 }
